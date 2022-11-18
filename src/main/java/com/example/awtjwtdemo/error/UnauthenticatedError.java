94b0a9e8ca6a640572e8326e2d602ec8eb2244ab
ThetWai924 <thetwai924@gmail.com>
2022-11-18 11:02:23 +0630

    Initial commit

---
+++ src/main/java/com/example/awtjwtdemo/error/UnauthenticatedError.java
@@ -0,0 +1,10 @@
+package com.example.awtjwtdemo.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UnauthenticatedError extends ResponseStatusException {
+    public UnauthenticatedError() {
+        super(HttpStatus.UNAUTHORIZED,"Authenticated Error");
+    }
+}
